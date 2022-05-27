04c4a4f226bcdfa0fad8f3edc38ac39b67da1bd5
Haseeb hanif <haseeb.idevation@gmail.com>
2022-05-27 19:10:51 +0500

    Update ChatController.php

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
 use App\Models\Chat;
 use Illuminate\Http\Request;
 
@@ -54,7 +55,7 @@ class ChatController extends Controller
                     $data['messages'][$key]['room_id']      = $message->room_id;
                     $data['messages'][$key]['sender_id']    = $message->sender_id;
                     $data['messages'][$key]['message']      = $message->message;
-                    $data['messages'][$key]['created_at']   = $message->created_at;
+                    $data['messages'][$key]['created_at']   = $this->time_elapsed_string($message->created_at); ;
                 }
 
                 return response()->json(['status' => true, 'data' => $data]);
@@ -63,5 +64,34 @@ class ChatController extends Controller
         }catch(\Exception $e){
             return response()->json(['status' => false, 'message' => $e->getMessage()]);
         }
+    }
+
+    function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+    
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+    
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+    
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
     }
 }
