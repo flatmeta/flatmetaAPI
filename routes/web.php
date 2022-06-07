19f0d183ea353b7746effa23c926fe0976053cff
Haseeb hanif <haseeb.idevation@gmail.com>
2022-06-07 16:45:58 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::any('/CreateProd', 'App\Http\Controllers\OrdersController@CreateProd');
+Route::any('/CreatePlan', 'App\Http\Controllers\OrdersController@CreatePlan');
 Route::any('/PurchaseTile', 'App\Http\Controllers\OrdersController@PurchaseTile');
 Route::any('/GetProdById', 'App\Http\Controllers\OrdersController@GetProdById');
 Route::any('/GetPlanById', 'App\Http\Controllers\OrdersController@GetPlanById');
